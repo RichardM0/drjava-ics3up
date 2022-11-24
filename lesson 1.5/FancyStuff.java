feb2550ca82b4fa2d3af4ffade880fa2f1c0f1ea
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2022-11-24 18:15:47 -0500

    Update FancyStuff.java

--- lesson 1.5/FancyStuff.java
+++ lesson 1.5/FancyStuff.java
@@ -15,16 +15,16 @@ public class FancyStuff {
 // In the third line, It properly formats the string.
 // In the fourth line, it does everything up to index 3. W is index 0, E is index 1, L is index 2. Index 3 and up is not included.
 // In the fifth line, It has 3 lines of output. 
-//           The fifth line has 2 spaces of whitespace, done by %3. Each line is created by another \n
-//           The sixth line Moves the white space after the first whitespace, so after the 7 done by %-3.
-//           The seventh line replaces the first 2 whitespaces with 0, done by %03.
+//           The first has 2 spaces of whitespace, done by %3. Each line is created by another \n
+//           The second moves the white space after the first whitespace, so after the 7 done by %-3.
+//           The third replaces the first 2 whitespaces with 0, done by %03.
 //           From this, I can conclude that in strings, after %, if you have a phrase or sequence with 3 keys, the first key is what you would replace the whitespace with,
 //           the second key being the number of spots you would like to replace with (not inclusive), and the third being the datatype replaced with. 
 
 // Along with the sixth line, the eigth line has 3 lines of output as well.
-//           The eigth line is rounded to 3 decimals, because of %.3f. and broken into a new line with \n
-//           The ninth line is slightly indented due %9, and then rounded to 3 decimals because of .3f.
-//           The tenth line rounds to two decimal places because of %1.2f.
+//           The first is rounded to 3 decimals, because of %.3f. and broken into a new line with \n
+//           The second is slightly indented due %9, and then rounded to 3 decimals because of .3f.
+//           The third rounds to two decimal places because of %1.2f.
 
 // With ints, in %12s, 3 whitespaces are added which is found by 12/3 - 1.
 // We can see this in %9s, when 2 whitespaces are added which is also found by 9/3 - 1.
