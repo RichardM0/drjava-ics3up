0d7d6835bbc98c06a1b190b6d4df171703204b73
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2022-12-02 19:14:25 -0500

    Update PowerJavaDoc.java

--- lesson 2.2/PowerJavaDoc.java
+++ lesson 2.2/PowerJavaDoc.java
@@ -1,3 +1,11 @@
+/**
+* function that returns a number raised to the power of another number
+*
+* @param  base  the number that is being exponentially increased
+* @param  expo  the number of times that number is being exponentially increased
+* @return       the exponential increase of a base and its exponent
+* @see          result
+*/
 public class Power{
   public static void main(String[] args){
     System.out.println(iterativePower(3, 4));
