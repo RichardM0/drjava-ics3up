843d03d52beeed32ed29f9ae21989933a110f67b
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2022-12-11 17:08:33 -0500

    Add files via upload

---
+++ lesson 2.7/RectangleTest2.java
@@ -0,0 +1,13 @@
+public class RectangleTest2 {
+    public static void main(String[] args){
+        Rectangle2 a = new Rectangle2();
+        System.out.println(a);
+
+        Rectangle2 b = new Rectangle2(-4, -3, -2, -1, false);
+        System.out.println(b);
+
+        Rectangle2 c = new Rectangle2(1,2,3,4,true);
+        System.out.println(c);
+
+    }
+}
