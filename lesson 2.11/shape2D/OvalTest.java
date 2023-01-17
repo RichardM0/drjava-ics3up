58185356ecece5d30686de09329cec26eec96fbd
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2023-01-16 21:11:14 -0500

    Add files via upload

---
+++ lesson 2.11/shape2D/OvalTest.java
@@ -0,0 +1,9 @@
+public class OvalTest {
+    public static void main(String[] args){
+        Oval a = new Oval(0,10,0,6, false);
+        System.out.println(a.getWidth());
+        System.out.println(a.getLength());
+        System.out.println(a.calcArea());
+        System.out.println(a.isCircle());
+    }
+}
