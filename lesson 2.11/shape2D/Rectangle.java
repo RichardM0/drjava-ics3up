df3ff2be4c0e17fef42749005df2dca16507bfc0
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2023-01-16 21:52:49 -0500

    Add files via upload

---
+++ lesson 2.11/shape2D/Rectangle.java
@@ -0,0 +1,57 @@
+package shape2D;
+
+import shape2D.FillableShape;
+
+public class Rectangle extends FillableShape{
+    private static int numRectangles = 0;
+
+    public Rectangle(){
+        numRectangles++;
+    }
+    public Rectangle(int x1, int x2, int y1, int y2, boolean filled){
+        super(x1,x2,y1,y2,filled);
+        if(x1<0){
+            System.err.println("Coordinate: " + x1 + " is less than 0 (negative), set to default: 0");
+            x1 = 0;
+        }
+        if(x2<0){
+            System.err.println("Coordinate: " + x2 + " is less than 0 (negative), set to default: 0");
+            x2 = 0;
+        }
+        if(y1<0){
+            System.err.println("Coordinate: " + y1 + " is less than 0 (negative), set to default: 0");
+            y1 = 0;
+        }
+        if(y2<0){
+            System.err.println("Coordinate: " + y2 + " is less than 0 (negative), set to default: 0");
+            y2 = 0;
+        }
+        numRectangles++;
+    }
+
+    public int getNumRectangles(){
+        return numRectangles;
+    }
+
+    public double calcArea(){
+        return (double)(super.getLength())*(double)(super.getWidth());
+    }
+      
+    public boolean isOverLapping(Rectangle other){
+        if((super.getUpperLeftX() > other.getBottomRightX()) || (super.getBottomRightY() > other.getUpperLeftY())){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+
+    }
+    @Override
+    public String toString(){
+
+        return super.toString() +"\n"+"Rectangle with length: " + super.getLength() + " and width: " + super.getWidth();
+    }
+
+
+}
